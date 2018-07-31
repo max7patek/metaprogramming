8099574eb02aca2d1d51c3ce8ecfcf9f38fd6bdc
Maxwell Patek <max7patek@gmail.com>
2018-07-31 15:15:12 -0400

    Finished draft of Decorators

--- misc/code_examples/debug_decorator.py
+++ misc/code_examples/debug_decorator.py
@@ -1,8 +1,50 @@
 
+
+# imports:
+
+import inspect
+
+
+
+# decorators:
+
 def debug(func):
     print('decorating', func.__name__)
     def wrapper(*args, **kwargs):
         print( func.__name__, 'being called on', args, kwargs)
-        func(*args, **kwargs)
+        return func(*args, **kwargs)
     return wrapper
 
+
+def debug_prefix(prefix):
+    print('receiving prefix:', prefix)
+    def debug(func):
+        print('decorating', func.__name__)
+        def wrapper(*args, **kwargs):
+            print(prefix, func.__name__, 'being called on', args, kwargs)
+            return func(*args, **kwargs)
+        return wrapper
+    return debug
+
+
+def debug_class(cls):
+    print('decorating', cls.__name__)
+    class wrapper(*cls.__bases__):
+        pass
+    for key, val in inspect.getmembers(cls, predicate=inspect.isfunction):
+        setattr(wrapper, key, debug(val))
+    return wrapper
+
+
+
+
+# tests:
+
+@debug_prefix('***')
+def foo(a, b):
+    print(a + b)
+    
+@debug_class
+class bar(object):
+    def foo(self, a , b):
+        print(a + b)
