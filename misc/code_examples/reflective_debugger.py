0ad8923bbfbf00ea7bde9e6185e0806dcf78c4f0
Maxwell Patek <max7patek@gmail.com>
2018-08-13 11:32:51 -0400

    Added code for the Reflective Debugger and a slide set stub.

---
+++ misc/code_examples/reflective_debugger.py
@@ -0,0 +1,104 @@
+
+# imports
+
+import inspect
+
+import sys
+
+from pprint import pprint
+
+import functools
+
+
+
+# tracer code
+
+def _get_source(obj):
+    try:
+        return inspect.getsourcelines(obj)
+    except OSError:
+        return None
+
+
+def upon_call(frame, event, arg):
+    return print_everything
+
+
+def print_line(line, is_current):
+    prefix = ' '
+    if is_current:
+        prefix = '>'
+    print(prefix, line, end='')
+
+def is_dunder(string):
+    return len(string) >= 2 and string[:2] == '__' and string[-2:] == '__'
+
+def set_vars(string, globals, locals):
+    arr = string.split('=')
+    arr = list(map(str.strip, arr))
+    if len(arr) == 2:
+        value = eval(arr[1], globals, locals)
+        if arr[0] in locals:
+            locals[arr[0]] = value
+        else:
+            globals[arr[0]] = value
+    
+
+
+def print_everything(frame, event, arg):
+    source = _get_source(frame.f_code)
+    if source is not None:
+        if event == 'return':
+            print('returning')
+            return
+        print('code:')
+        for index, line in enumerate(source[0]):
+            print_line(line, index == frame.f_lineno - frame.f_code.co_firstlineno)
+        print('globals:')
+        for i in frame.f_globals:
+            if not is_dunder(i):
+                print('   ', i, '=', frame.f_globals[i])
+        print('locals:')
+        for i in frame.f_locals:
+            print('   ', i, '=', frame.f_locals[i])
+        print('callstack:')
+        for i in reversed(inspect.getouterframes(frame)):
+            print('   ',i.function + ':', 
+                  i.code_context[0] if i.code_context is not None else '\n',
+                  end='')
+        string = input('set an already declared variable or just hit enter: ')
+        set_vars(string, frame.f_globals, frame.f_locals)
+
+
+# interface
+
+def start():
+    sys.settrace(upon_call)
+
+
+def nop(frame, event, arg):
+    return None
+
+end = functools.partial(sys.settrace, nop)
+
+
+def debug(callme):
+    def func(*args, **kwargs):
+        start()
+        callme(*args, **kwargs)
+        end()
+    return func
+
+# testing and demo
+
+if __name__ == '__main__':
+    global foo, bar
+    import itertools
+    
+    def foo(a, b):
+        c = a + b
+        print(c)
+
+    def bar(*args):
+        for i in itertools.combinations(args, 2):
+            foo(*i)
