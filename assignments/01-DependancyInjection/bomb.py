6a4ddd9c7bf009d40f2a2a26d23dd9a1985c0a82
Maxwell Patek <max7patek@gmail.com>
2018-09-09 21:41:16 -0400

    Added runner file and renforced the bomb from ninja attacks.

--- assignments/01-DependancyInjection/bomb.py
+++ assignments/01-DependancyInjection/bomb.py
@@ -3,8 +3,8 @@ class explosives:
         print('BOOOOOOOM')
         exit(1)
 
-
 class bomb(explosives):
     def trigger(self):
+        assert isinstance(self, bomb)
         print("the code is ", 'abcde')
         super().trigger()
---
+++ assignments/01-DependancyInjection/runner.py
@@ -0,0 +1,4 @@
+import sys
+for key in sys.modules:
+    sys.modules[key] = None
+import <computing id>_diffuser
