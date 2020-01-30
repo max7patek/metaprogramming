8a251c343ce89f052dbdb32a56507fd867f9bada
Ubuntu <ubuntu@ip-172-31-91-69.ec2.internal>
2020-01-30 19:54:39 +0000

    fixed for newer python versions

--- assignments/01-DependancyInjection/runner.py
+++ assignments/01-DependancyInjection/runner.py
@@ -5,6 +5,7 @@ Run this file to test your bomb diffuser.
 import sys
 import builtins
 for key in sys.modules:
-    sys.modules[key] = None
+    if key != "_io":
+        sys.modules[key] = None
 import bomb
 import diffuser
