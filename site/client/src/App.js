481eb3dd60eb90a8c9317976b56496680ff5021c
Maxwell Patek <max7patek@gmail.com>
2020-06-08 18:48:12 +0000

    Adding Grommet

--- site/client/src/App.js
+++ site/client/src/App.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Box, Button, Heading, Collapsible, Grommet } from 'grommet';
+import { Notification } from 'grommet-icons';
+
+const theme = {
+    global: {
+      colors: {
+        brand: '#228BE6'
+      },
+      font: {
+        family: 'Roboto',
+        size: '18px',
+        height: '20px',
+      },
+    },
+  };
+
+  const AppBar = (props) => (
+    <Box
+      tag='header'
+      direction='row'
+      align='center'
+      justify='between'
+      background='brand'
+      pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+      elevation='medium'
+      style={{ zIndex: '1' }}
+      {...props}
+    />
+  );
 
 class App extends Component {
+
+
     constructor(props) {
         super(props);
         this.state = {
             apiResponse: "",
-            dbResponse: ""
+            dbResponse: "",
+            showSidebar: false
         };
     }
 
@@ -35,14 +65,36 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
+            <Grommet theme={theme} full>
+              <Box fill>
+                <AppBar>
+                  <Heading level='3' margin='none'>My App</Heading>
+                  <Button 
+                    icon={<Notification />} 
+                    onClick={() => this.setState({showSidebar: !this.state.showSidebar})} 
+                  />
+                </AppBar>
+                <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+                  <Collapsible direction="horizontal" open={this.state.showSidebar}>
+                    <Box
+                      flex
+                      width='medium'
+                      background='light-2'
+                      elevation='small'
+                      align='center'
+                      justify='center'
+                    >
+                      sidebar
+                    </Box>
+                  </Collapsible>
+                  <Box flex align='center' justify='center'>
+                    app body
+                  </Box>
+                </Box>
                 <p className="App-intro">{this.state.apiResponse}</p>
                 <p className="App-intro">{this.state.dbResponse}</p>
-            </div>
+              </Box>
+            </Grommet>
         );
     }
 }
